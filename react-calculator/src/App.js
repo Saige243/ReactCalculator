0e0ff090ddfcd8e5df590430215bf30e0f12d717
Saige Cross <saigecrossdev@gmail.com>
2021-08-26 23:24:58 -0500

    second commit

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -5,12 +5,65 @@ import KeypadComponent from './KeypadComponent';
 import ResultComponent from './ResultComponent';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+
+    if(button === "="){
+        this.calculate()
+    }
+
+    else if(button === "C"){
+        this.reset()
+    }
+    
+    else {
+        this.setState({
+            result: this.state.result + button
+        })
+    }
+};
+
+
+calculate = () => {
+  try{
+    this.setState({
+      // eslint-disable-next-line
+      result: (eval(this.state.result) || "" ) + ""
+    })
+  } catch (e) {
+    this.setState({
+      result: 'error'
+    })
+  }
+};
+
+reset = () => {
+  this.setState({
+    result: ""
+  })
+};
+
+backspace = () => {
+  this.setState({
+    result: this.state.result.slice(0, -1)
+  })
+};
+
+  
   render(){
     return (
     <div>
-      <div className='calc-body'>
+      <div className='calcBody'>
         <h1>Simple Calculator</h1>
-        <KeypadComponent/>
+        <ResultComponent result={this.state.result}/>
+        <KeypadComponent onClick={this.onClick}/>
       </div>
     </div>
     );
@@ -19,4 +72,3 @@ class App extends Component {
 
 export default App;
 
-// WHY ISN"T THE H1 RENDERING?
